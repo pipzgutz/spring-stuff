8102034199b19d6150134c750bfa32c10ac18dd7
Philip Gutierrez <pgutierrez@owens.com>
2017-05-29 22:05:46 -0400

    change RegistrationView to be online in the request scope

--- spring-boot-online-registration-example/src/main/java/com/flowetc/springstuff/registration/view/RegistrationView.java
+++ spring-boot-online-registration-example/src/main/java/com/flowetc/springstuff/registration/view/RegistrationView.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import java.util.List;
 
 /**
@@ -13,6 +14,7 @@ import java.util.List;
  * @version 1.0
  * @since May 25, 2017
  */
+@RequestScoped
 @Component
 public class RegistrationView {
     private Registration registration;
